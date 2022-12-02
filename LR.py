cd3eb8cb1ed3af3efb65953eaea3a6478dad9677
DianaD <diana_draghiciu@yahoo.com>
2022-12-02 17:41:59 +0200

    Configuration for lab6

---
+++ LR.py
@@ -0,0 +1,13 @@
+class LR:
+    def __init__(self, grammar):
+        self.grammar = grammar
+
+    # TODO
+    def closure(self):
+        pass
+
+    def goTo(self):
+        pass
+
+    def canonicalCol(self):
+        pass
