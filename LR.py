7ecd53a290a494925aa5510239c3d1f86942944f
DianaD <diana_draghiciu@yahoo.com>
2022-12-03 18:07:50 +0200

    CanonicalCol and GoTo functions are implemented

--- LR.py
+++ LR.py
@@ -1,12 +1,20 @@
 class LR:
     def __init__(self, grammar):
         self.grammar = grammar
-        self.closure = {}
-        self.canonicalCollection = []
+        self.grammar.productions['S`'] = []
+        self.grammar.productions['S`'].append('S')  # add augmented production
+        self.canonicalCollection = {} # productions, but with the dot added
+        self.createCanCol()
 
-    # TODO
-    def closure(self):
+    def createCanCol(self):
+        for production in self.grammar.productions:
+            self.canonicalCollection[production] = []
+            for elem in self.grammar.productions[production]:
+                self.canonicalCollection[production].append('.' + elem)
+
+    def computeClosure(self, list):
         """
+        I-state
         repeat
             for any [A -> α.Bβ] in C do
                 for any B -> γ in P do
@@ -16,18 +24,58 @@ class LR:
                 end for
             end for
         until C stops changing
-        :return:
+        :return: C = closure(I);
         """
-        pass
+        # inputState: [a.A, b.]
+        items = list.copy()
+        while True:
+            for elem in items.values():
+                index = elem.find('.')
+                if index != -1:
+                    if index < len(elem):  # dot not at end
+                        symbol = elem[index + 1]
+                        if symbol in self.grammar.non_terminals:  # found dot non-terminal
+                            for prod in self.grammar.productions.keys():
+                                if prod == symbol and prod not in items:
+                                    items.append(prod)
+                            if list == items:
+                                break
+                    else:  # dot at end
+                        continue
+        #TODO
+        # return ??
 
     def goTo(self, state, symbol):
         """
         goto(s, X) = closure({[A → αX.β]|[A → α.Xβ] ∈ s})
-        :param state:
-        :param symbol:
-        :return:
+        :param state: initial state
+        :param symbol: non-terminal to find in initial state productions
+        :return: a closure
         """
-        pass
+        # ex state=S, symbol=A we find .AA and move the dot and end up with A.A and call closure
+        items = []
+        for elem in self.canonicalCollection[state]:
+            index = elem.find('.')
+            if index < len(elem):  # dot not at end
+                if elem[index + 1] == symbol:
+                    # move dot
+                    aux = ""
+                    i = 0
+                    while i < (len(elem)):
+                        if i == index:
+                            aux += elem[i + 1]
+                            aux += elem[i]
+                            i += 2
+                        else:
+                            aux += elem[i]
+                            i += 1
+                    items.append(aux)
+            else:
+                continue
+        if items:
+            return self.computeClosure(items)
+        else:
+            return []
 
     def canonicalCol(self):
         """
@@ -46,7 +94,7 @@ class LR:
             for s in aux_canonical_collection:
                 for X in self.grammar.non_terminals + self.grammar.terminals:
                     result = self.goTo(s, X)
-                    if result != [] and result not in aux_canonical_collection:
-                        self.canonicalCollection.append(result)
+                    if result != [] and result not in aux_canonical_collection.values():
+                        self.canonicalCollection[s].append(result)
             if aux_canonical_collection == self.canonicalCollection:  # C stopped changing
                 break
